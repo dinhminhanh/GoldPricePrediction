ff87873a61c67176f2c6544920d9e2037f64614b
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-04-12 12:16:41 +0700

    crawler

---
+++ Crawl/src/driver/AMyDriverContext.java
@@ -0,0 +1,10 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AMyDriverContext {
+	
+	protected WebDriver driver ;
+	public abstract WebDriver getDriver() ;
+	
+}
