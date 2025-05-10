dbd55808fdc4d47e489969faedc16f83405aac6c
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-05-10 23:23:22 +0700

    update dockerfile

--- crawler/src/main/java/com/example/crawler/driver/ChromeDriverContext.java
+++ crawler/src/main/java/com/example/crawler/driver/ChromeDriverContext.java
@@ -2,12 +2,15 @@ package com.example.crawler.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverContext extends AMyDriverContext {
 	
 	public ChromeDriverContext() {
-		System.setProperty("webdriver.chrome.driver", "src\\main\\java\\com\\example\\crawler\\lib\\chromedriver.exe");
-		driver = new ChromeDriver() ;
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+		driver = new ChromeDriver(options);
 	}
 
 	@Override
