8fa9eee000a23063945c2e58029698ac2f7efe2e
JosephD0310 <dangnguyenhoang8@gmail.com>
2025-05-11 08:36:38 +0700

    fix selenium

--- crawler/src/main/java/com/example/crawler/driver/ChromeDriverContext.java
+++ crawler/src/main/java/com/example/crawler/driver/ChromeDriverContext.java
@@ -4,40 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.*;
-import java.nio.file.Files;
-
 public class ChromeDriverContext extends AMyDriverContext {
 
-    public ChromeDriverContext() {
-        try {
-            // Tải chromedriver từ resource và ghi ra file tạm
-            InputStream input = getClass().getClassLoader().getResourceAsStream("bin/chromedriver");
-            if (input == null) {
-                throw new FileNotFoundException("Chromedriver not found in resources/bin/");
-            }
+	public ChromeDriverContext() {
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
 
-            File tempDriver = Files.createTempFile("chromedriver", "").toFile();
-            tempDriver.deleteOnExit();
-            try (OutputStream out = new FileOutputStream(tempDriver)) {
-                input.transferTo(out);
-            }
-            tempDriver.setExecutable(true);
+	    // Không cần set webdriver.chrome.driver nếu dùng selenium/standalone-chrome
+	    driver = new ChromeDriver(options);
+	}
 
-            // Cấu hình system property để Selenium sử dụng chromedriver này
-            System.setProperty("webdriver.chrome.driver", tempDriver.getAbsolutePath());
-
-            // Cấu hình ChromeOptions
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless"); // Chạy không giao diện
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-
-            driver = new ChromeDriver(options);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to initialize ChromeDriver", e);
-        }
-    }
 
     @Override
     public WebDriver getDriver() {
